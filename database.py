6ee28b2f6bdc70d8b7252a927b68eaff84adde69
Miguel Ramos <83405368+coisoetal@users.noreply.github.com>
2026-03-20 17:24:27 +0100

    Update database.py

--- database.py
+++ database.py
@@ -6,7 +6,7 @@ import os
 
 load_dotenv()
 
-DATABASE_URL = os.getenv("DATABASE_URL")
+DATABASE_URL = os.getenv("postgresql://postgres:vxrsmQPpwsuWpfESwuuoHkHCiUryeBDZ@ballast.proxy.rlwy.net:55901/railway")
 
 engine = create_engine(DATABASE_URL)
 SessionLocal = sessionmaker(autocommit=False, autoflush=False, bind=engine)
